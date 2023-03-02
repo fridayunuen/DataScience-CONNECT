88b95d27db8854a5fe500d7a6571c427eb880b7b
Frida Colín <fridayunuen99@gmail.com>
2023-03-01 21:16:31 -0600

    EdicionReporteSAP

--- ReporteFotosSAP/code/ReporteSubirFoto.py
+++ ReporteFotosSAP/code/ReporteSubirFoto.py
@@ -3,27 +3,52 @@ import pandas as pd
 
 path = os.getcwd()
 path_input = path.replace("code", "input")
-path_output = r'S:\OMNI\HerramientasCode\MappingDiario'
+
 path_contrasenas = r'C:\Users\fcolin\Desktop\input\CredencialesConnect.json'
 path_sap_reporte = r'S:\OMNI\HerramientasCode\MappingDiario\input'
+path_code = os.getcwd()
+path_output = path_code.replace("code", "output")
+# select a file with an interactive dialog
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
+# select a file with an interactive dialog
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename_SAP = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+# close the dialog
+Tk().destroy()
 # DRIVE ----------------------------------------------------------------------------------------------
-Drive = pd.read_csv(r"S:\OMNI\HerramientasCode\MappingDiario\Mapping.csv")
+print('Leyendo Drive...')
+try:
+    Drive = pd.read_csv(r"S:\OMNI\HerramientasCode\MappingDiario\Mapping.csv")
+except:
+    print('No se puede acceder a la carpeta S:, revise la conexión a la red')
+    exit()
+
 Drive.sort_values(by=['creation_date'], ascending=False, inplace=True)
 drive = Drive[["sku", "path"]]
 drive = drive.drop_duplicates(subset="sku", keep="first")
 drive.reset_index(drop=True, inplace=True)
 drive["sku"] = drive["sku"].astype(str)
-
+drive
 # Producto dado de alta en SAP ------------------------------------------------
-ProductosSAP = pd.read_csv(path_sap_reporte + "\SAP.csv", sep=";", header=1)
-ProductosSAP["sku"] = (ProductosSAP["code[unique=true]"].str[:7]+ProductosSAP["code[unique=true]"].str[8:]).str[:10].astype(str)
-ProductosSAP = ProductosSAP.drop_duplicates(subset="sku", keep="first")
+print("Leyendo archivo SAP...")
+ProductosSAP = pd.read_csv(filename_SAP, sep=";", header=1)
+#ProductosSAP = pd.read_csv(path_sap_reporte + "\SAP.csv", sep=";", header=1)
+ProductosSAP['code[unique=true]'] = ProductosSAP['code[unique=true]'].str.replace("_", "")
 
-df_sap = ProductosSAP[ProductosSAP['picture(code)[collection-delimiter=|]'].isna()]
-df_sap.loc[:,'SAP'] = 'SinFoto'
-df_sap = df_sap[['sku', 'SAP']]
-df_sap.reset_index(drop=True, inplace=True)
+ProductosSAP['len_code'] = ProductosSAP['code[unique=true]'].str.len()
+ProductosSAP = ProductosSAP[ProductosSAP['len_code'] == 13]
+ProductosSAP['sku'] = ProductosSAP['code[unique=true]'].str[:10]
+ProductosSAP.drop_duplicates(subset="sku", keep="first", inplace=True)
+ProductosSAP
+ProductosSAP[~ProductosSAP['picture(code)[collection-delimiter=|]'].isna()]
+# crear una nueva columna en la que si no tiene foto, ponga "Sin foto"
+ProductosSAP['SAP'] = ProductosSAP['picture(code)[collection-delimiter=|]'].apply(lambda x: "Sin foto" if pd.isna(x) else "Con foto")
+
+df_sap = ProductosSAP[['sku', 'SAP']]
+df_sap
+
 # INVENTARIOS --------------------------------------------------------------------------------------------
 print("Leyendo base de datos... extrayendo tallas")
 from sqlalchemy.engine import URL
@@ -80,12 +105,41 @@ df_query['Total'] = df_query.sum(axis=1)
 df_query = df_query[df_query['Total']>0]
 df_query.reset_index(inplace=True)
 
-# UNION ----------------------------------------------------------------------------------------------
-df = pd.merge(df_sap, df_query, on="sku", how="left")
+df = pd.merge(df_query, df_sap, on="sku", how="left")
+
 df['Total'].fillna(0, inplace=True)
 df['Total'] = df['Total'].astype(int)
 df = df[df['Total']>0]
 df = pd.merge(df, drive, on="sku", how="left")
-df.to_excel(path_output + "\ProductosSinFoto.xlsx", index=False)
+df
+df = df[df['SAP'] != 'Con foto']
+sin_foto = df[df['path'].isna()]
+sin_foto
 
-print('Proceso terminado con exito....')
+# fotos que no estan en SAP, necesita crearse un archivo para migrar
+migrar = df[(df['SAP'].isna()) & (~df['path'].isna())]
+subir = df[(~df['SAP'].isna()) & (~df['path'].isna())]
+subir
+
+sin_foto.reset_index(drop=True, inplace=True)
+migrar
+prod_migrar = migrar['sku'].str[:7].unique()
+prod_migrar = pd.DataFrame(prod_migrar)
+prod_migrar['E-COMMERCE'] = 'E-COMMERCE'
+prod_migrar
+
+#prod_migrar.to_excel(path_output + "\Migrar.xlsx", index=False, header=False)
+#sin_foto.to_excel(path_output + "\SinFotoDriveCONNECT.xlsx", index=False)
+#subir.to_excel(path_output + "\SubirFoto.xlsx", index=False)
+
+def crear_excel(df, titulo, header=True):
+    if df.empty:
+        print("No hay productos en" , titulo, " :)")
+    else:
+        df.to_excel(path_output + titulo , index=False, header=header)
+crear_excel(df, "\ReporteFotos.xlsx")
+crear_excel(migrar, "\Migrar.xlsx", False)
+crear_excel(sin_foto, "\SinFotoDriveCONNECT.xlsx")
+crear_excel(subir, "\SubirFoto.xlsx")
+
+print('Proceso terminado con exito :)')
